af6feadab3986f992c32da70901498881c9640ae
sk1nnyking06 <laidaniel06@gmail.com>
2024-03-04 12:16:54 -0600

    Initiating the flask app and Routing Login to Register page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/login')
 def home():
-    
-    return 'Hello, Flask World!'
+
+    return render_template('Login.html')
+
+@app.route('/register')
+def register():
+    return render_template('Register.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
